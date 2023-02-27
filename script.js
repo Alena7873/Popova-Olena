e4b6b71fc0ab373c5689981cc9b900b058fef967
Alena <magazinnna@gmail.com>
2023-02-28 00:41:50 +0200

    хедор 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const image = document.getElementById("myImage");
+const text = document.getElementById("myText");
+
+image.addEventListener("click", () => {
+    text.textContent = "Україна місто вулиця буд ";
+});
+
