e16d6113ed284adf4eb7849675ecc6cc9d7a35dc
Alena <magazinnna@gmail.com>
2023-04-01 23:42:39 +0300

    медичний масаж

--- script.js
+++ script.js
@@ -156,5 +156,13 @@ window.onload = function () {
     });
 }
 
-
+// like kn
+$('.hero-btn').click(function () {
+    if (!$('.hero-btn').parent().hasClass('active')) {
+        $(this).parent().stop().addClass('active');
+        setTimeout(function () {
+            $('.hero-btn').parent().removeClass('active');
+        }, 2000);
+    }
+});
 
